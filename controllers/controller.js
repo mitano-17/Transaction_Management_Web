9da308b32fe1aa42fdd03fbbd48e2f261464f151
Nicole Jocson <nicpjocson@gmail.com>
2024-04-08 23:24:35 +0800

    Create controller.js

---
+++ controllers/controller.js
@@ -0,0 +1,15 @@
+const db = require('../models/db.js');
+
+const controller = {
+    postInsert: async function (req, res) {
+
+    },
+    
+    postUpdate: async function (req, res) {
+
+    },
+
+    postSelect: async function (req, res) {
+
+    }
+}
