3aead4a573e8a3d7bce569f0e34425cd0c15da4d
Nicole Jocson <nicpjocson@gmail.com>
2024-04-09 00:18:57 +0800

    Added postUpdate

--- controllers/controller.js
+++ controllers/controller.js
@@ -32,7 +32,77 @@ const controller = {
     },
     
     postUpdate: async function(req, res) {
+        const { appointmentId } = req.params;
+        const {
+            old_patientId, 
+            new_patientId, 
+            old_clinicId, 
+            new_clinicId, 
+            old_doctorId, 
+            new_doctorId, 
+            old_queuedDate, 
+            new_queuedDate, 
+            old_startTime, 
+            new_startTime, 
+            old_endTime, 
+            new_endTime, 
+            old_appointmentType, 
+            new_appointmentType, 
+            old_isVirtual, 
+            new_isVirtual, 
+        } = req.body;
 
+        var patientId = "";
+        var clinicId = "";
+        var doctorId = "";
+        var queuedDate = "";
+        var startTime = "";
+        var endTime = "";
+        var appointmentType = "";
+        var isVirtual = "";
+
+        if (old_patientId != new_patientId) {
+            patientId = new_patientId;
+        }
+        if (old_clinicId != new_clinicId) {
+            clinicId = new_clinicId;
+        }
+        if (old_doctorId != new_doctorId) {
+            doctorId = new_doctorId;
+        }
+        if (old_queuedDate != new_queuedDate) {
+            queuedDate = new_queuedDate;
+        }
+        if (old_startTime != new_startTime) {
+            startTime = new_startTime;
+        }
+        if (old_endTime != new_endTime) {
+            endTime = new_endTime;
+        }
+        if (old_appointmentType != new_appointmentType) {
+            appointmentType = new_appointmentType;
+        }
+        if (old_isVirtual != new_isVirtual) {
+            isVirtual = new_isVirtual;
+        }
+
+        try {
+            var result = await db.update_query(appointmentId, patientId, clinicId, doctorId, queuedDate, startTime, endTime, appointmentType, isVirtual);
+            
+            var data = {
+                result: result,
+                status: true,
+                msg: "Update appointment success."
+            };
+            // this means successful
+            res.send(data);
+        } catch (err) {
+            var data = {
+                status: false,
+                msg: "Update appointment failed."
+            }
+            res.send(data)
+        }
     },
 
     postSelect: async function(req, res) {
---
+++ models/db.js
@@ -0,0 +1,15 @@
+const nodes = require('./nodes.js');
+
+const db_operations = {
+    select_query: async function() {
+
+    },
+
+    insert_query: async function() {
+
+    }, 
+
+    update_query: async function() {
+
+    }
+}
