0a4f8f4704dd1948981b54068c9668397536f5ee
Nicole Jocson <nicpjocson@gmail.com>
2024-04-10 01:26:10 +0800

    Using index.html as UI

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const controller = require("../controllers/controller.js");
 
-const app = express();
+const app = express.Router();
 
-app.get('/', controller.getIndex);
-app.post('/insert', controller.postInsert);
-app.put('/update/:id', controller.postUpdate);
-app.post('/search', controller.postSelect);
+app.get('/', (req, res) => {
+    const indexPath = path.join(__dirname, '../views/index.html');
+    res.sendFile(indexPath);
+});
+// app.post('/insert', controller.postInsert);
+// app.put('/update/:id', controller.postUpdate);
+// app.post('/search', controller.postSelect);
 
 module.exports = app;
