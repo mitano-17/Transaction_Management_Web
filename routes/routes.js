ca811dce28efafb4af85fbba0ebb4380ee208ed8
Nicole Jocson <nicpjocson@gmail.com>
2024-04-09 19:54:00 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const controller = require("../controllers/controller.js");
 const app = express.Router();
+const controller = require("../controllers/controller.js");
 
-app.post("/insert", controller.postInsert);
-app.put("/update/:id", controller.postUpdate);
-app.post("/search", controller.postSelect);
+app.get('/', controller.getIndex);
+app.post('/insert', controller.postInsert);
+app.put('/update/:id', controller.postUpdate);
+app.post('/search', controller.postSelect);
 
-app.get("*", function(req, res) { res.render("error", {}); } );
 module.exports = app;
