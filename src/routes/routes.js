bed70600e6fb199b81ac635158caa05ef3d979bf
Nicole Jocson <nicpjocson@gmail.com>
2024-04-09 22:25:04 +0800

    Changed express.Router() to express()?

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const app = express.Router();
+const express = require('express');
 const controller = require("../controllers/controller.js");
+
+const app = express();
 
 app.get('/', controller.getIndex);
 app.post('/insert', controller.postInsert);
