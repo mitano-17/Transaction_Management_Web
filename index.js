7ea128eca0f2c46e00cc343a24ed164b4a32b26c
Kate Justine U. Ermitano <80894347+mitano-17@users.noreply.github.com>
2024-04-10 17:06:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-const dotenv = require(`dotenv`).config();;
+const dotenv = require('dotenv');
 const express = require('express');
-const hbs = require('hbs');
-//const path = require('path');
-
-// Modules
-const connect = require('./src/models/db.js');
-const { node_utils } = require('./src/models/nodes.js');
-const replicator = require('./src/models/replicator.js');
-const sync = require(`./src/models/sync.js`);
-
-const routes = require('./src/routes/routes.js');
-
 const app = express();
+const mysql = require('mysql');
 
-app.use(express.urlencoded({ extended: false }));
+dotenv.config();
 
-app.use(express.static('./public'));
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-app.use('/', routes);
-
-// const port = process.env.SERVER_PORT;
-// const hostname = process.env.HOSTNAME;
-const port = process.env.SERVER_PORT || 8080;
-const hostname = 'localhost';
-
+// Route for the root URL ('/')
 app.get('/', (req, res) => {
-    res.send('test');
+    res.send('test'); // This will never be executed because 'index.html' will be served first
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}`);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+const host = 'localhost';
+const port = '8080';
+
+var connection = mysql.createConnection({
+  host: host,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: port,  
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// export const node1Pool = mysql.createPool({
+//     host: process.env.HOSTNAME,
+//     port: process.env.PORT1,
+//     user: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+// });
+
+// export const node2Pool = mysql.createPool({
+//     host: process.env.HOSTNAME,
+//     port: process.env.PORT2,
+//     user: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+// });
+
+// export const node3Pool = mysql.createPool({
+//     host: process.env.HOSTNAME,
+//     port: process.env.PORT3,
+//     user: process.env.USERNAME,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+// });
